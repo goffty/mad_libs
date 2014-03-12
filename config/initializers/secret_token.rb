09080410f0931cbc027a263ebbd18d3ffa38c7a8
Tyler Goff <tgoff@lsoc.org>
2014-03-12 18:41:13 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MadLibs::Application.config.secret_key_base = '522f5ceba0e54753fbbddf76c7c1450b54d9d8f5397898d80e3ca8753af31a9b2d002cc9bc81d4a258ed188ccb6179a80c5cf97b8a58f16a2b49349c3fa2df7e'
